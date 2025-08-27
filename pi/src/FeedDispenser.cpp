aedee10cfd79a027fde549c13e1a15200fda5c29
Aneek-Mubarak-Pi <aneek.mubarak@gmail.com>
2025-08-27 12:56:11 -0600

    Add servo power driver to enable power only during dispensing

--- pi/src/FeedDispenser.cpp
+++ pi/src/FeedDispenser.cpp
@@ -10,38 +10,47 @@
 #define OFF 0
 
 
-FeedDispenser::FeedDispenser(gpiod_line* line, int start_position) // second para is optional
-    :m_line(line),m_pulse_duration_us(start_position){}
-
-FeedDispenser::FeedDispenser(gpiod_line* line)
-    :FeedDispenser(line,1000){}
 
 
+FeedDispenser::FeedDispenser(gpiod_line* line, gpiod_line* driver_enable_line,  int start_position) // second para is optional
+    :m_line(line),m_pulse_duration_us(start_position),m_driver_line(driver_enable_line){}
 
-void FeedDispenser::move_servo(){ // cretaes a pulse on the output pin for the desired pulse length
+FeedDispenser::FeedDispenser(gpiod_line* line, gpiod_line* driver_enable_line)
+    :FeedDispenser(line,driver_enable_line,1000){}
+
+
+
+// cretaes a pulse on the output pin (from 1-2.5ms) to move to the desired location.
+void FeedDispenser::move_servo(){ 
     gpiod_line_set_value(m_line,ON);
     std::this_thread::sleep_for(std::chrono::microseconds(m_pulse_duration_us));
     gpiod_line_set_value(m_line,OFF);
     std::this_thread::sleep_for(std::chrono::microseconds(PULSE_PERIOD_US - m_pulse_duration_us));
 }
 
+// the servo takes pulses from 1ms to 2.5ms, this function starts at 1ms (0 deg) and moves the servo by 180 deg by gradually changing the pulse to 2.5ms and back to 0 deg
 void FeedDispenser::dispense(){
-
+    std::cout << "Dispensing...\n";
+    gpiod_line_set_value(m_driver_line,ON);
     for(;;){
         if(m_pulse_duration_us >= 2500){
             move_direction = 0; //go reverse
         }
 
         move_servo();
+        std::cout << m_pulse_duration_us << "\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        move_direction ? m_pulse_duration_us+=100 : m_pulse_duration_us-=100;
+        move_direction ? m_pulse_duration_us+=250 : m_pulse_duration_us-=250;
 
         if(m_pulse_duration_us < 1000){
-            break;
+            m_pulse_duration_us = 1000;
+            move_direction = 1;
+            break;        
         }
-
-
     }
+    
+    gpiod_line_set_value(m_driver_line,OFF);
+    std::cout<< "driver_off\n";
 }
 
 
--- pi/src/feeder_main.cpp
+++ pi/src/feeder_main.cpp
@@ -4,14 +4,26 @@
 #include <string_view>
 
 
+
 #include <gpiod.h>
-// #include <thread>
-// #include <chrono>
+#include <thread>
+#include <chrono>
+
+#define ON 1
+#define OFF 0
+
+//~ void motor_driver(gpiod_line* line, int val){
+        //~ gpiod_line_set_value(line,val);
+        
+//~ }
 
 int main(){
 
     const char* chipname = "gpiochip4";
     const unsigned int servo_line_num = 22;
+    const unsigned int motor_driver_en_line_num= 17;
+
+    
 
     gpiod_chip* chip = gpiod_chip_open_by_name(chipname);
     if(!chip){
@@ -20,6 +32,8 @@ int main(){
     }
 
     gpiod_line* servoLine = gpiod_chip_get_line(chip,servo_line_num);
+    gpiod_line* motor_driver_en_line = gpiod_chip_get_line(chip,motor_driver_en_line_num);
+
 
     if(!servoLine){
         std::cerr << "Failed to get line\n";
@@ -28,21 +42,46 @@ int main(){
     }
 
     if (gpiod_line_request_output(servoLine, "servo", 0) < 0) {
-        std::cerr << "Failed to request line 1 as output\n";
+        std::cerr << "Failed to request line as output\n";
+        gpiod_chip_close(chip);
+        return 1;
+    }
+    
+    
+    if(!motor_driver_en_line){
+        std::cerr << "Failed to get line\n";
         gpiod_chip_close(chip);
         return 1;
     }
 
-    std::cout<<"Aneek\n";
+    if (gpiod_line_request_output(motor_driver_en_line, "motor_driver", 0) < 0) {
+        std::cerr << "Failed to request line as output\n";
+        gpiod_chip_close(chip);
+        return 1;
+    }
+    
+    
 
-    FeedDispenser d1 {servoLine}
+    std::cout<<"Testing\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    FeedDispenser d1 {servoLine,motor_driver_en_line};
     d1.dispense();
 
 
 
 
 
+
+    
+    
+
+
+
+
+
     gpiod_line_release(servoLine);
+    gpiod_line_release(motor_driver_en_line);
+
 
     gpiod_chip_close(chip);
     return 0;
